6b2c506689a6233874bd4a835e6d7550aefa6102
Andrea Gianarda <mrandre.epfl@gmail.com>
2018-11-06 01:07:15 +0000

    Parse header of file

--- include/parse_idx.h
+++ include/parse_idx.h
@@ -7,10 +7,22 @@
 */
 
 /**
+ * @brief Rename char type as byte for readability reasons
+ *
+ */
+typedef char byte;
+
+/**
  * @brief Maximum number of character in every line of parsed file
  *
  */
-#define LINE_MAX_LENGTH 20
+#define LINE_MAX_LENGTH 2
+
+/**
+ * @brief A byte constains 8 bits
+ *
+ */
+#define BIT_IN_BYTE 8
 
 /** @defgroup ParseIDX Parse IDX Functions
  *  Functions concerning image
@@ -36,5 +48,29 @@ void parse_idx (char * test_set, char * train_set, char * test_label, char * tra
  * Parse IDX file and store data into structure
  */
 void parse_idx_file (char * filename);
+
+/** 
+ * @brief Function: int read_header (FILE * fid)
+ *
+ * \param fid: file description of the file to parse
+ *
+ * Read the value of one field of the header and convert little endian to big endian
+ */
+int read_header(FILE * fid);
+
+/** 
+ * @brief Function: void parse_header (FILE * fid)
+ *
+ * \param fid: file description of the file to parse
+ *
+ * Parse the header of the IDX file.
+ * Extract the following informations:
+ *   -# Number of data type
+ *   -# Number of dimensions
+ *   -# Dimensions
+ */
+void parse_header(FILE * fid);
+
+
 
 /** @} */ // End of ParseIDX group
--- src/parse_idx.c
+++ src/parse_idx.c
@@ -30,19 +30,71 @@ void parse_idx_file (char * filename) {
 	}
 
 	if (fid!=NULL) {
+		log_info("================================================================\n", filename);
 		log_info("Parsing file %s\n", filename);
-		char * fileline = NULL;
-		fileline = (char *) malloc(LINE_MAX_LENGTH*sizeof(char));
-		if (fileline==NULL) {
-			LOG_ERROR("Can't allocate memory for variable to store parsed filelines at %s, function %s, line %0d\n", __FILE__, __func__, __LINE__);
-		}
+		log_info("================================================================\n", filename);
+		parse_header(fid);
+		log_info("Parsing complete.\nClosing file %s\n", filename);
 
-		while (fgets(fileline, LINE_MAX_LENGTH, fid) == NULL) {
-
-		}
-
-		free(fileline);
 	}
+
 
 	file_close(fid);
 }
+
+int read_header(FILE * fid) {
+	int value_le = 0;
+	fread(&value_le, sizeof(value_le), 1, fid);
+
+	// Convert from little endian to big endian
+	int value_be=0;
+	const int int_bits = BIT_IN_BYTE*sizeof(value_le);
+	const int mask = 0xFF;
+	for(int byte = 0; byte < (int) sizeof(value_le); byte++) {
+		value_be+=(((value_le>>(int_bits-(BIT_IN_BYTE*(byte+1)))) & mask) << (BIT_IN_BYTE*byte));
+	}
+
+	return value_be;
+}
+
+void parse_header(FILE * fid) {
+
+	// Variable storing the 32 bits fo the magic number
+	int magic_number;
+	// Variable storing the 32 bits fo the magic number grouped as bytes (8 bits)
+	byte * magic_number_bytes = NULL;
+	magic_number_bytes = (byte *) malloc(sizeof(int)*sizeof(byte));
+	if (magic_number_bytes == NULL) {
+		LOG_ERROR("Can't allocate memory for magic number at %s, function %s, line %0d\n", __FILE__, __func__, __LINE__);
+	}
+
+	magic_number = read_header(fid);
+	memcpy(magic_number_bytes, &magic_number, sizeof(int));
+
+	byte no_dims;
+	// Number of dimensions is always the LSB of the magic number (i.e. byte0)
+	no_dims = magic_number_bytes[0];
+
+	byte data_type;
+	// Number of dimensions is always the LSB of the magic number (i.e. byte0)
+	data_type = magic_number_bytes[1];
+
+	log_info("Magic number %0d\n    -> number of dimensions %0d\n    -> data type %0d\n", magic_number, no_dims, data_type);
+
+	int * dimensions = NULL;
+	dimensions = (int *) malloc(no_dims*sizeof(int));
+	if (dimensions == NULL) {
+		LOG_ERROR("Can't allocate memory for dimensions of data at %s, function %s, line %0d\n", __FILE__, __func__, __LINE__);
+	}
+
+	// Loop through dimensions
+	for (int dim = 0; dim < no_dims; dim++) {
+		dimensions[dim] = read_header(fid);
+		log_info("Dimension %0d: %0d\n", dim, dimensions[dim]);
+	}
+
+	free (dimensions);
+	free (magic_number_bytes);
+}
+
+
