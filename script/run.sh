06c55d37a980c2dcae8520b390a4182d49af4ec1
Andrea Gianarda <mrandre.epfl@gmail.com>
2018-11-13 00:37:11 +0000

    Allow to run 3 targets: doc (-d), tests (-t) and valgrind (-v)

--- script/run.sh
+++ script/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Shell script settings
+valgrind=0
+tests=0
+doc=0
+
+
+# Make file settings
 EXENAME=ml
 EXEDIR=bin
 
@@ -7,8 +14,9 @@ LOGDIR=log
 DEBUGLOG=debug.log
 COMPLOG=compile.log
 DOCLOG=doc.log
-CLEANLOG=clean.log
-VALGRINDLOG=valgrind.log
+EXELOG=${EXENAME}.log
+VALGRINDTESLOG=valgrind_tes.log
+VALGRINDTELLOG=valgrind_tel.log
 EXEVALGRINDLOG=${EXENAME}_valgrind.log
 
 DATASET_DIR=dataset/mnist
@@ -20,6 +28,37 @@ TEST_LABEL=t10k-labels-idx1-ubyte
 VERBOSITY=LOW
 
 CEXTRAFLAGS=-DENABLE_ASSERTIONS
+
+if [$# -lt 1]; then
+	usage
+fi
+
+for args
+do
+	case "$1" in
+		--test|-t)
+			tests=1
+			shift 1
+			;;
+		--doc|-d)
+			tests=1
+			shift 1
+			;;
+		--valgrind|-v)
+			valgrind=1
+			shift 1
+			;;
+		--help|-h)
+			usage
+			exit 0
+			;;
+		?*)
+			echo "Illegal argument $1"
+			usage
+			exit 0
+			;;
+	esac
+done
 
 echo "Run script variables"
 echo "\n========================================================================="
@@ -40,11 +79,18 @@ echo "LOGS"
 echo "========================================================================="
 echo "--> Logfile directory: ${LOGDIRR}"
 echo "--> Debug logfile name: ${DEBUGLOG}"
-echo "--> Compile logfile name: ${COMPLOG}"
-echo "--> Documentation logfile name: ${DOCLOG}"
-echo "--> Clean logfile name: ${CLEANLOG}"
-echo "--> Valgrind logfile name: ${VALGRINDLOG}"
-echo "--> Valgrind executable logfile name: ${EXEVALGRINDLOG}"
+if [ ${tests} -eq 1 ]; then
+	echo "--> Compile logfile name: ${COMPLOG}"
+	echo "--> Executable logfile name: ${EXELOG}"
+fi
+if [ ${doc} -eq 1 ]; then
+	echo "--> Documentation logfile name: ${DOCLOG}"
+fi
+if [ ${valgrind} -eq 1 ]; then
+	echo "--> Valgrind test set logfile name: ${VALGRINDTESLOG}"
+	echo "--> Valgrind test label logfile name: ${VALGRINDTELLOG}"
+	echo "--> Valgrind executable logfile name: ${EXEVALGRINDLOG}"
+fi
 
 echo "\n========================================================================="
 echo "Clean workspace"
@@ -62,38 +108,50 @@ echo "\n========================================================================
 echo "Makefile variables"
 echo "========================================================================="
 (set -x; \
- make debug LOG_DIR=${LOGDIR} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} EXTRAFLAGS=${CEXTRAFLAGS} > ${LOGDIR}/${DEBUGLOG})
+ make debug LOG_DIR=${LOGDIR} LOGFILENAME=${EXELOG} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} EXTRAFLAGS=${CEXTRAFLAGS} > ${LOGDIR}/${DEBUGLOG})
 
-echo "\n========================================================================="
-echo "Compile sources"
-echo "========================================================================="
-(set -x; \
- make all LOG_DIR=${LOGDIR} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} VERBOSITY=${VERBOSITY} EXTRAFLAGS=${CEXTRAFLAGS} > ${LOGDIR}/${COMPLOG})
+if [ ${tests} -eq 1 ]; then
+	echo "\n========================================================================="
+	echo "Compile sources"
+	echo "========================================================================="
+	(set -x; \
+	 make all LOG_DIR=${LOGDIR} LOGFILENAME=${EXELOG} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} VERBOSITY=${VERBOSITY} EXTRAFLAGS=${CEXTRAFLAGS} > ${LOGDIR}/${COMPLOG})
 
-echo "\n========================================================================="
-echo "Compile documetation"
-echo "========================================================================="
-(set -x; \
- make doc LOG_DIR=${LOGDIR} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} EXTRAFLAGS=${CEXTRAFLAGS} > ${LOGDIR}/${DOCLOG})
+	echo "\n========================================================================="
+	echo "Run program"
+	echo "========================================================================="
+	echo "START:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}"
+	(set -x; \
+	 ./bin/ml -tel ${DATASET_DIR}/${TEST_LABEL})
+	echo "COMPLETED:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}"
+	echo "START:Test parsing test set file ${DATASET_DIR}/${TEST_SET}"
+	(set -x; \
+	 ./bin/ml -tes ${DATASET_DIR}/${TEST_SET})
+	echo "COMPLETED:Test parsing test set file ${DATASET_DIR}/${TEST_SET}"
+	echo "START:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}, test set file ${DATASET_DIR}/${TEST_SET}, training label file ${DATASET_DIR}/${TRAIN_LABEL}, training set file ${DATASET_DIR}/${TRAIN_SET}"
+	(set -x; \
+	 ./bin/ml -tel ${DATASET_DIR}/${TEST_LABEL} -tes ${DATASET_DIR}/${TEST_SET} -trl ${DATASET_DIR}/${TRAIN_LABEL} -trs ${DATASET_DIR}/${TRAIN_SET})
+	echo "COMPLETED:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}, test set file ${DATASET_DIR}/${TEST_SET}, training label file ${DATASET_DIR}/${TRAIN_LABEL}, training set file ${DATASET_DIR}/${TRAIN_SET}"
+fi
 
-echo "\n========================================================================="
-echo "Run program"
-echo "========================================================================="
-echo "START:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}"
-(set -x; \
- ./bin/ml -tel ${DATASET_DIR}/${TEST_LABEL})
-echo "COMPLETED:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}"
-echo "START:Test parsing test set file ${DATASET_DIR}/${TEST_SET}"
-(set -x; \
- ./bin/ml -tes ${DATASET_DIR}/${TEST_SET})
-echo "COMPLETED:Test parsing test set file ${DATASET_DIR}/${TEST_SET}"
-echo "START:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}, test set file ${DATASET_DIR}/${TEST_SET}, training label file ${DATASET_DIR}/${TRAIN_LABEL}, training set file ${DATASET_DIR}/${TRAIN_SET}"
-(set -x; \
- ./bin/ml -tel ${DATASET_DIR}/${TEST_LABEL} -tes ${DATASET_DIR}/${TEST_SET} -trl ${DATASET_DIR}/${TRAIN_LABEL} -trs ${DATASET_DIR}/${TRAIN_SET})
-echo "COMPLETED:Test parsing test label file ${DATASET_DIR}/${TEST_LABEL}, test set file ${DATASET_DIR}/${TEST_SET}, training label file ${DATASET_DIR}/${TRAIN_LABEL}, training set file ${DATASET_DIR}/${TRAIN_SET}"
+if [ ${doc} -eq 1 ]; then
+	echo "\n========================================================================="
+	echo "Compile documetation"
+	echo "========================================================================="
+	(set -x; \
+	 make doc LOG_DIR=${LOGDIR} LOGFILENAME=${EXELOG} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} EXTRAFLAGS=${CEXTRAFLAGS} > ${LOGDIR}/${DOCLOG})
+fi
 
-echo "\n========================================================================="
-echo "Run valgrind"
-echo "========================================================================="
-(set -x; \
- make valgrind LOG_DIR=${LOGDIR} LOGFILENAME=${EXEVALGRINDLOG} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} VALGRINDLOGFILENAME=${VALGRINDLOG} VALGRINDEXEARGS="-tes ${DATASET_DIR}/${TEST_SET} -trl ${DATASET_DIR}/${TRAIN_LABEL} -trs ${DATASET_DIR}/${TRAIN_SET}")
+if [ ${valgrind} -eq 1 ]; then
+	echo "\n========================================================================="
+	echo "Run valgrind"
+	echo "========================================================================="
+	echo "START:Valgrind parsing test label file ${DATASET_DIR}/${TEST_LABEL}"
+	(set -x; \
+	 make valgrind LOG_DIR=${LOGDIR} LOGFILENAME=${EXEVALGRINDLOG} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} VALGRINDLOGFILENAME=${VALGRINDTELLOG} VALGRINDEXEARGS="-tel ${DATASET_DIR}/${TEST_LABEL}")
+	echo "COMPLETED:Valgrind parsing test label file ${DATASET_DIR}/${TEST_LABEL}"
+	echo "START:Valgrind parsing test set file ${DATASET_DIR}/${TEST_SET}"
+	(set -x; \
+	 make valgrind LOG_DIR=${LOGDIR} LOGFILENAME=${EXEVALGRINDLOG} EXE_NAME=${EXENAME} BIN_DIR=${EXEDIR} VALGRINDLOGFILENAME=${VALGRINDTESLOG} VALGRINDEXEARGS="-tes ${DATASET_DIR}/${TEST_SET}")
+	echo "COMPLETED:Valgrind parsing test set file ${DATASET_DIR}/${TEST_SET}"
+fi
