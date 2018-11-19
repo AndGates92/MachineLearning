b5726c0170394ae8b8bf83254290e807f1cb89f9
Andrea Gianarda <mrandre.epfl@gmail.com>
2018-11-19 22:57:34 +0000

    Add template for feedforward stage fo neural network

--- include/feedforward_stage.h
+++ include/feedforward_stage.h
@@ -10,17 +10,17 @@
 
 #include "data.h"
 /** 
- * @brief Function: void feedforward_stage (data_t * data_set, double * weights, double * biases, double ** output_node_val, int * layers_dim)
+ * @brief Function: void feedforward_stage (double * weights, double * biases, int * layers_dim, elementdatatype * input_data, elementdatatype ** output_node_val)
  *
  *
  * \param weights: weights of neural network layers
  * \param bias: bias of neural network
- * \param data_set:  data structure to extract dimensions of each layer of neural network
- * \param output_node_val:  result from the neural network. It is a vector of values between zero and one
  * \param layers_dim:  array storing dimensions of every layer in the neural network
+ * \param input_data:  array of one element to process
+ * \param output_node_val:  result from the neural network. It is a vector of values between zero and one
  *
  * Initialize weight and bias of neural network
  */
-void feedforward_stage (data_t * data_set, elementdatatype_t * weights, elementdatatype_t * biases, elementdatatype ** output_node_val, int * layers_dim):
+void feedforward_stage (double * weights, double * biases, int * layers_dim, elementdatatype * input_data, elementdatatype ** output_node_val):
 
 #endif // FEEDFORWARD_STAGE_H
---
+++ src/feedforward_stage.c
@@ -0,0 +1,18 @@
+/**
+ * @copyright
+ * @file feedforward_stage.c
+ * @author Andrea Gianarda
+ * @date 19th of November 2018
+ * @brief Functions related to the feedforward stage of the neural network
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "log.h"
+#include "feedforward_stage.h"
+#include "utility.h"
+
+void feedforward_stage (double * weights, double * biases, int * layers_dim, elementdatatype_t * input_data, double ** output_node_val) {
+
+}
