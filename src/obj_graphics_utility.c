db815488713a89a917a773569ba0f5e3c6a230ea
Andrea Gianarda <mrandre.epfl@gmail.com>
2019-01-09 22:19:22 +0000

    Allow reshaping window by reducing the dimensions of the window

--- src/obj_graphics_utility.c
+++ src/obj_graphics_utility.c
@@ -57,7 +57,7 @@ unsigned char * reshape_img(int width_orig, int height_orig, double win_width, d
 	}
 
 	int win_img_height_ratio_int = 0;
-	win_img_height_ratio_int = (int)(win_img_height_ratio);
+	win_img_height_ratio_int = (int)(floor(win_img_height_ratio));
 
 	double win_img_height_ratio_dec = 0.0;
 	if (win_img_height_ratio == (double)(win_img_height_ratio_int)) {
@@ -73,11 +73,12 @@ unsigned char * reshape_img(int width_orig, int height_orig, double win_width, d
 	int width_corr = 0;
 	bool add_extra_col = false;
 
-	LOG_INFO(DEBUG,"[Reshape image] original image width %0d height %0d \n", width_orig,height_orig);
-	LOG_INFO(DEBUG,"[Reshape image] reshape image width %0lf height %0lf \n", win_width,win_height);
-	LOG_INFO(DEBUG,"[Reshape image] ratio width %0lf height %0lf \n", win_img_width_ratio,win_img_height_ratio);
-	LOG_INFO(DEBUG,"[Reshape image] Integer part of ratio width %0d height %0d \n", win_img_width_ratio_int,win_img_height_ratio_int);
-	LOG_INFO(DEBUG,"[Reshape image] Inverse of decimal part of ratio width %0lf height %0lf \n", win_img_width_ratio_dec,win_img_height_ratio_dec);
+	LOG_INFO(DEBUG,"[Reshape image] Dimensions reduced: width %s height %s\n", bool_to_str(width_reduced),bool_to_str(height_reduced));
+	LOG_INFO(DEBUG,"[Reshape image] original image width %0d height %0d\n", width_orig,height_orig);
+	LOG_INFO(DEBUG,"[Reshape image] reshape image width %0lf height %0lf\n", win_width,win_height);
+	LOG_INFO(DEBUG,"[Reshape image] ratio width %0lf height %0lf\n", win_img_width_ratio,win_img_height_ratio);
+	LOG_INFO(DEBUG,"[Reshape image] Integer part of ratio width %0d height %0d\n", win_img_width_ratio_int,win_img_height_ratio_int);
+	LOG_INFO(DEBUG,"[Reshape image] Inverse of decimal part of ratio width %0lf height %0lf\n", win_img_width_ratio_dec,win_img_height_ratio_dec);
 
 	for (int width_idx = 0; width_idx < (int)win_width; width_idx++) {
 
@@ -96,11 +97,11 @@ unsigned char * reshape_img(int width_orig, int height_orig, double win_width, d
 				*(img + NO_COLOURS*(height_idx*(int)win_width + width_idx) + colour_idx) = *(img_orig + NO_COLOURS*(orig_height_idx*width_orig + orig_width_idx) + colour_idx);
 			}
 
-			orig_height_idx = orig_idx_mgmt (height_reduced, height_orig, height_idx, orig_height_idx, &height_corr, win_img_height_ratio, win_img_height_ratio_int, win_img_height_ratio_dec, &add_extra_line);
+			orig_height_idx = orig_idx_mgmt(height_reduced, height_orig, height_idx, orig_height_idx, &height_corr, win_img_height_ratio, win_img_height_ratio_int, win_img_height_ratio_dec, &add_extra_line);
 
 		}
 
-		orig_width_idx = orig_idx_mgmt (width_reduced, width_orig, width_idx, orig_width_idx, &width_corr, win_img_width_ratio, win_img_width_ratio_int, win_img_width_ratio_dec, &add_extra_col);
+		orig_width_idx = orig_idx_mgmt(width_reduced, width_orig, width_idx, orig_width_idx, &width_corr, win_img_width_ratio, win_img_width_ratio_int, win_img_width_ratio_dec, &add_extra_col);
 
 	}
 
@@ -109,24 +110,31 @@ unsigned char * reshape_img(int width_orig, int height_orig, double win_width, d
 
 int orig_idx_mgmt (bool reduced, int orig, int reshaped_idx, int orig_idx, int * corr, double ratio, int ratio_int, double ratio_dec, bool * add_extra) {
 
-		if (reduced == false) {
-			int idx_corr = 0;
-			idx_corr = (reshaped_idx - *corr);
-			if ((idx_corr % ratio_int) == (ratio_int - 1)) {
-				// Increase only if no need to add extra column, extra line was not added the previous time around or ratio has no decimal part
-				if (((idx_corr % ((int) ratio_dec)) < (((int) ratio_dec) - 1)) || (*add_extra == true) || (ratio == (double)(ratio_int))) {
-					if (orig_idx < (orig - 1)) {
-						orig_idx++;
-					}
-					*add_extra = false;
-				} else {
-					(*corr)++;
-					*add_extra = true;
+	int idx_corr = 0;
+	idx_corr = (reshaped_idx - *corr);
+
+	if (reduced == false) {
+		if ((idx_corr % ratio_int) == (ratio_int - 1)) {
+			// Increase only if no need to add extra column, extra line was not added the previous time around or ratio has no decimal part
+			if (((idx_corr % ((int) ratio_dec)) < (((int) ratio_dec) - 1)) || (*add_extra == true) || (ratio == (double)(ratio_int))) {
+				if (orig_idx < (orig - 1)) {
+					orig_idx++;
 				}
+				*add_extra = false;
+			} else {
+				(*corr)++;
+				*add_extra = true;
 			}
-		} else {
-			
 		}
+	} else {
+		orig_idx+=ratio;
+		// Increase only if no need to add extra column, extra line was not added the previous time around or ratio has no decimal part
+		if ((idx_corr % ((int) ratio_dec)) == (((int) ratio_dec) - 1)) {
+			if (orig_idx < (orig - 1)) {
+				orig_idx++;
+			}
+		}
+	}
 
 	return orig_idx;
 }
